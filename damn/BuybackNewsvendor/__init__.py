b62a48ee53c26772382fc110ef8be26582eef224
Daniels <baz7755@uni-hamburg.de>
2023-01-17 16:40:31 +0100

    second commit

---
+++ damn/BuybackNewsvendor/__init__.py
@@ -0,0 +1,154 @@
+from otree.api import *
+
+class Calcu:
+
+    def newsvendor(P, S, Q, D, C, BB, W):
+        Rest = Q-D
+        if Rest >= 0:
+            Revenue = D*P + Rest*(S+BB)
+            Buyback = (BB-S) * Rest
+        else:
+            Revenue = Q*P
+            Buyback = 0
+        Profit = Revenue - Q * W
+        Profits = Q*(W-C)-Buyback
+
+        results = {
+            "PF": Profit,
+            "PFS":Profits
+
+        }
+        return results
+
+class C(BaseConstants):
+    NAME_IN_URL = 'BuybackNewsvendor'
+    PLAYERS_PER_GROUP = 2
+    NUM_ROUNDS = 6
+    INSTRUCTIONS_TEMPLATE = 'BuybackNewsvendor/instructions.html'
+    # Initial amount allocated to each player
+    DEMAND = [148, 184, 83, 137, 215, 173, 97]
+    MEAN = 140
+    STANDARDDIV = 40
+    PROFIT = 20
+    SALVAGE = 2
+    WHOLESALE = 10
+    COST = 5
+    OPTIMALSOLUTION = [10, 112, 239, 89]
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+
+    buyback_reduction = models.FloatField(initial=0,
+         label="Please input the buyback price")
+    quantity = models.IntegerField(initial=0, label="Please input how many units you want to order")
+    buyback = models.FloatField(initial=0)
+
+
+
+class Player(BasePlayer):
+
+    buysell = models.StringField()
+    profit = models.IntegerField()
+# FUNCTIONS
+
+
+
+def set_payoffs(group: Group):
+    p1 = group.get_player_by_id(1)
+    p2 = group.get_player_by_id(2)
+    total_costs = Calcu.newsvendor(P=C.PROFIT, S=C.SALVAGE, Q=group.quantity, D=C.DEMAND[group.round_number], C=C.COST, BB=group.buyback_reduction, W=C.WHOLESALE)
+    ProfitB = total_costs['PF']
+    ProfitS = total_costs['PFS']
+    p1.profit = int(ProfitS)
+    p2.profit = int(ProfitB)
+
+
+# PAGES
+class instructions(Page):
+    pass
+
+class Introduction(Page):
+    def vars_for_template(player: Player):
+        if player.id_in_group == 1:
+
+            player.buysell = "Supplier"
+
+        else:
+
+            player.buysell = "Buyer"
+
+
+class Send(Page):
+    """This page is only for P1
+    P1 sends amount (all, some, or none) to P2
+    This amount is tripled by experimenter,
+    i.e if sent amount by P1 is 5, amount received by P2 is 15"""
+
+    form_model = 'group'
+    form_fields = ['buyback_reduction']
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.id_in_group == 1
+
+
+class SendBackWaitPage(WaitPage):
+    pass
+
+
+class SendBack(Page):
+    """This page is only for P2
+    P2 sends back some amount (of the tripled amount received) to P1"""
+
+    form_model = 'group'
+    form_fields = ['quantity']
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.id_in_group == 2
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        group = player.group
+
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = set_payoffs
+
+
+class Results(Page):
+    """This page displays the earnings of each player"""
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        group = player.group
+
+
+        total_costs = Calcu.newsvendor(P=C.PROFIT, S=C.SALVAGE, Q=group.quantity, D=C.DEMAND[group.round_number],
+                                       C=C.COST, BB=group.buyback_reduction, W=C.WHOLESALE)
+        ProfitB = total_costs['PF']
+        ProfitS = total_costs['PFS']
+        DEMAND = C.DEMAND[group.round_number]
+
+        return dict(
+                    PF=int(ProfitB),
+                    PFS=int(ProfitS),
+                    Dem=DEMAND,
+                    PFSC=int(ProfitB)+int(ProfitS)
+                    )
+
+
+
+page_sequence = [
+    Introduction,
+    Send,
+    SendBackWaitPage,
+    SendBack,
+    ResultsWaitPage,
+    Results,
+]
---
+++ damn/PureNewsvendor/__init__.py
@@ -0,0 +1,100 @@
+from otree.api import *
+
+class Calcu:
+
+    def newsvendor(P, S, Q, D, C):
+        Rest = Q-D
+        if Rest >= 0:
+            Revenue = D*P + Rest*S
+        else:
+            Revenue = Q*P
+
+        Profit = Revenue - Q * C
+
+
+        results = {
+            "PF": Profit,
+
+        }
+        return results
+
+class C(BaseConstants):
+    NAME_IN_URL = 'PureNewsvendor'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 6
+    INSTRUCTIONS_TEMPLATE = 'PureNewsvendor/instructions.html'
+    # Initial amount allocated to each player
+    ENDOWMENT = cu(100)
+    MULTIPLIER = 3
+
+    DEMAND = [148, 184, 83, 137, 215, 173, 97]
+    MEAN = 140
+    STANDARDDIV = 40
+    PROFIT = 20
+    SALVAGE = 2
+    COST = 10
+    OPTIMALSOLUTION = [10, 112, 239, 89]
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Player(BasePlayer):
+
+    quantity = models.IntegerField(initial=0, label="Please input how many units you want to order")
+    ordering_cost = models.IntegerField()
+    buysell = models.StringField()
+    profit = models.IntegerField()
+# FUNCTIONS
+
+
+class Group(BaseGroup):
+    pass
+
+
+#def set_payoffs(player:Player):
+
+ #   total_costs = Calcu.eoq(s=C.REORDERCOSTB, ss=C.REORDERCOSTS, Q=player.quantity, H=C.HOLDINGCOST, D=C.DEMAND,
+ #                           C=0)
+  #  tcb = total_costs['TC']
+ #   player.payoff = C.PROFIT*C.DEMAND - tcb
+
+ #PAGES
+
+
+class instructions(Page):
+    pass
+
+
+class Introduction(Page):
+    pass
+
+class order(Page):
+
+    form_model = 'player'
+    form_fields = ['quantity']
+
+
+
+class Results(Page):
+    """This page displays the earnings of each player"""
+    @staticmethod
+
+    def vars_for_template(player: Player):
+        total_costs = Calcu.newsvendor(P=C.PROFIT, S=C.SALVAGE, Q=player.quantity, C=C.COST, D=C.DEMAND[player.round_number])
+        Profit = int(total_costs['PF'])
+        player.profit = Profit
+        DEMAND = C.DEMAND[player.round_number]
+
+        return dict(
+                    TC=Profit,
+                    Dem=DEMAND,
+
+                    )
+
+
+page_sequence = [
+    Introduction,
+    order,
+    Results,
+]
